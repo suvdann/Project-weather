edaa3afe85dfc7492cf5f050a7e4d3e224f52d6f
suvdann <m.erdenesuwd2021@gmail.com>
2025-04-21 22:11:54 +0800

    background icon

---
+++ src/app/components/Ellipse.jsx
@@ -0,0 +1,6 @@
+const Ellipse=()=>{
+    return (
+        <div className="border-solid"> border</div>
+    )
+}
+export default Ellipse
--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,18 @@
 const Home = () => {
   return (
-    <div>
-      {" "}
-      <div
-        className={`bg-black background-color: var(--color-black); h-screen
-height: 100vh;`}
-      >
-        vvvvvvvv
+    <div className="flex h-screen relative  ">
+     
+      <div className="w-1/2 bg-white rounded-s-md flex justify-end  flex items-center justify-center  ">
+      <img src="/Images/Vector.png" className="w-{43.28889465332031} h-{86.00000762939453} flex absolute items-center "/>
       </div>
-      <div
-        className={`bg-black background-color: var(--color-white); h-screen
-height: 100vh;`}
-      >
-        hhh
+      <div className="w-1/2 bg-black rounded-e-md flex justify-start  flex items-center justify-center  ">
+      <img src="/Images/icon.png " className="w-{43.28889465332031} h-{86.00000762939453} flex absolute items-center bg-white "/>
       </div>
+     <input></input>
+
+
     </div>
   );
 };
+
 export default Home;
