4863af8f7e42680e50726d4405c18a39753abbb6
25LP2729 25LP2729 <25LP2729@G401-CC0102.local>
2025-04-23 12:26:33 +0800

    angid

---
+++ src/app/_components/Input.jsx
@@ -0,0 +1,13 @@
+const Search = () => {
+  return <div>h</div>;
+};
+
+export default Search;
+
+{
+  /* <input
+  type="text"
+  placeholder="Search city"
+  className="w-full p-3 rounded-lg border focus:outline-none "
+/>; */
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,33 @@
-import Image from "next/image"
-import Card from "./_components/Card"
-import Dark from "./_components/dark"
+import Image from "next/image";
+import SunCard from "./_components/SunCard";
+import Dark from "./_components/dark";
+import Input from "./_components/Input";
 const Home = () => {
   return (
-    <div className="flex justify-center items-center
-     h-screen relative  ">
-     
+    <div
+      className="flex justify-center items-center
+     h-screen relative  "
+    >
       {/* white */}
-      <div className=" flex w-[800px] h-[1200px] bg-[#F3F4F6] rounded-s-md   flex items-center justify-center  ">
-    <Card />
+      <div className=" relative flex w-[800px] h-[1200px] bg-[#F3F4F6] rounded-s-md   flex items-center justify-center  ">
+        <Input />
+        <SunCard />
 
-        <img src="/Images/Vector.png" className="  " />
+        <img src="/Images/Vector.png" className=" absolute left-[750px] z-8 " />
       </div>
       {/* dugui gadna */}
-      
-      
+
       <div className="relative flex w-[800px] h-[1200px] bg-black rounded-e-md flex justify-start  items-center justify-center ">
         {/* white backgr*/}
-        <div className="z-6 outline-none w-[80px] h-[150px] bg-[#F3F4F6]  border-[100%] rounded-tr-full rounded-br-full "></div> 
-         {/* murui deed */}
-        <div className="z-6 absolute  outline-none w-[50px] h-[45px] bg-[#F3F4F6]   border-[100%] top-[490px]  "></div>
-        <div className="z-6 absolute  outline-none w-[51px] h-[48px] bg-black border-[100%] top-[490px] rounded-bl-full "></div>
+        <div className="z-6 outline-none w-[75px] h-[149px] bg-[#F3F4F6]  border-[100%] rounded-tr-full rounded-br-full "></div>
+        {/* murui deed */}
+        <div className="z-6 absolute  outline-none w-[51px] h-[44px] bg-[#F3F4F6]   border-[100%] top-[488px]  "></div>
+        <div className="z-6 absolute  outline-none w-[60px] h-[46px] bg-black border-[100%] top-[488px] rounded-bl-full "></div>
 
-
-          {/* murui */}
-          <div className="z-6 absolute  outline-none w-[76px] h-[66px] bg-[#F3F4F6] border-[100%]  top-[656.6px] "></div>
-          <div className="z-6 absolute  outline-none w-[80px] h-[67px] bg-black border-[100%]  top-[656.6px] rounded-tl-full "></div>
-    <Dark/>
+        {/* murui */}
+        <div className="z-6 absolute  outline-none w-[65px] h-[65px] bg-[#F3F4F6] border-[100%]  top-[655px] "></div>
+        <div className="z-6 absolute  outline-none w-[70px] h-[69px] bg-black border-[100%]  top-[653px] rounded-tl-full "></div>
+        <Dark />
         <img src="/Images/icon.png " className=" absolute z-6  " />
       </div>
     </div>
